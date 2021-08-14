666b6c8c845a1db6a1aa49a763434efaa31904a4
Alin Mr <almr.oss@outlook.com>
2021-08-14 17:15:03 +0300

    setup, .mk: t3st-e.t -> t3st-lib/*.t.0

--- t/t3st.mk
+++ t/t3st.mk
@@ -6,9 +6,10 @@ t3st-prove:
 	git -c t3st.prove-shells=$(T3ST_PROVE_SHELLS) t3st-prove
 
 new: new.t new-e.t
+	@echo '## Rename new.t and new-e.t and edit'
 
 new.t: t3st-lib/t3st-new.t.0
 	cp t3st-lib/t3st-new.t.0 new.t
 
-new-e.t: t3st-e.t
-	ln -s t3st-e.t new-e.t
+new-e.t: t3st-lib/t3st-e.t.0
+	ln -s t3st-lib/t3st-e.t.0 new-e.t
