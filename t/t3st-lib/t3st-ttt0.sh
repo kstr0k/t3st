afdd32ea986ee6eaf2aa41e3c116bbd8b9bb2611
Alin Mr <almr.oss@outlook.com>
2021-08-27 18:26:52 +0300

    ttt0: only _early gets $0

--- t/t3st-lib/t3st-ttt0.sh
+++ t/t3st-lib/t3st-ttt0.sh
@@ -5,8 +5,8 @@ set -u
 
 # each TTT__FUNC defaults to TTT__FUNC_0; can extend (override & call default)
 
-TTT__tfile_setup_0() {  # args: abs-path-to-t-file ...
-  . "${1%/*}"/t3st-lib/k9s0ke_t3st_lib.sh
+TTT__tfile_setup_0() {  # args: .t invocation
+  . "$TTT__tfile_mydirn"/t3st-lib/k9s0ke_t3st_lib.sh
   TTT()    { k9s0ke_t3st_one "$@"; }
   TTT_ee() { k9s0ke_t3st_one errexit=true  "$@"; }
   TTT_de() { k9s0ke_t3st_one errexit=false "$@"; }
@@ -20,23 +20,23 @@ TTT__tfile_setup() { TTT__tfile_setup_0 "$@"; }
 
 TTT__tfile_thelp_0() {
 cat <<EOHELP
-# Called as: '$*' (k9s0ke_t3st_g_errexit=${k9s0ke_t3st_g_errexit:-})
+# Called as: '$TTT__tfile_mypath $*' (k9s0ke_t3st_g_errexit=${k9s0ke_t3st_g_errexit:-})
 EOHELP
 }
 TTT__tfile_thelp() { TTT__tfile_thelp_0 "$@"; }
 
-TTT__tfile_early_0() {
+TTT__tfile_early_0() {  # args: $0 + .t invocation
   TTT__tfile_mypath=$1; shift
   case "$TTT__tfile_mypath" in (/*) ;; (*) TTT__tfile_mypath=$PWD/$TTT__tfile_mypath ;; esac
-  TTT__tfile_myname=${TTT__tfile_mypath#**/}
+  TTT__tfile_myname=${TTT__tfile_mypath##*/}
   TTT__tfile_mydirn=${TTT__tfile_mypath%/*}; TTT__tfile_mydirn=${TTT__tfile_mydirn:-/}
 }
 TTT__tfile_early() { TTT__tfile_early_0 "$@"; }
 
-TTT__tfile_runme_0() {  # args: $0 + "$@" from .t invocation
-  case "${2:-}" in
+TTT__tfile_runme_0() {  # args: .t invocation
+  case "${1:-}" in
     (--help|-h) shift
-      TTT__tfile_thelp "$TTT__tfile_mypath"; echo '1..0 # Skipped: help requested' ;;
+      TTT__tfile_thelp "$@"; echo '1..0 # Skipped: help requested' ;;
     (*)
       set -e; TTT__tfile_setup "$@"; set +e
       k9s0ke_t3st_enter
