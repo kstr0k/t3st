23938be427d60fded774b8b9ddb39802c3a6c553
Alin Mr <almr.oss@outlook.com>
2021-08-28 07:50:07 +0300

    ttt0: use ${1+"$@"} (posh)

--- t/t3st-lib/t3st-ttt0.sh
+++ t/t3st-lib/t3st-ttt0.sh
@@ -16,14 +16,16 @@ TTT__tfile_setup_0() {  # args: .t invocation
   }
   TTTnl=$k9s0ke_t3st_nl
 }
-TTT__tfile_setup() { TTT__tfile_setup_0 "$@"; }
+TTT__tfile_setup() { TTT__tfile_setup_0 ${1+"$@"}; }
+
+# ${1+"$@"}: work-around for shells (posh, old bash) that break $@ / $* when $# = 0
 
 TTT__tfile_thelp_0() {
 cat <<EOHELP
-# Called as: '$TTT__tfile_mypath $*' (k9s0ke_t3st_g_errexit=${k9s0ke_t3st_g_errexit:-})
+# Called as: '$TTT__tfile_mypath ${1+"$*"}' (k9s0ke_t3st_g_errexit=${k9s0ke_t3st_g_errexit:-})
 EOHELP
 }
-TTT__tfile_thelp() { TTT__tfile_thelp_0 "$@"; }
+TTT__tfile_thelp() { TTT__tfile_thelp_0 ${1+"$@"}; }
 
 TTT__tfile_early_0() {  # args: $0 + .t invocation
   TTT__tfile_mypath=$1; shift
@@ -31,21 +33,23 @@ TTT__tfile_early_0() {  # args: $0 + .t invocation
   TTT__tfile_myname=${TTT__tfile_mypath##*/}
   TTT__tfile_mydirn=${TTT__tfile_mypath%/*}; TTT__tfile_mydirn=${TTT__tfile_mydirn:-/}
 }
-TTT__tfile_early() { TTT__tfile_early_0 "$@"; }
+TTT__tfile_early() { TTT__tfile_early_0 ${1+"$@"}; }
 
 TTT__tfile_runme_0() {  # args: .t invocation
   case "${1:-}" in
     (--help|-h) shift
-      TTT__tfile_thelp "$@"; echo '1..0 # Skipped: help requested' ;;
+      TTT__tfile_thelp ${1+"$@"}; echo '1..0 # Skipped: help requested' ;;
     (--t3st-shell=*) local sh; sh="${1#*=}"; shift
-      eval exec "$sh" '"$TTT__tfile_mypath" "$@"' ;;
+      eval exec "$sh" '"$TTT__tfile_mypath" ${1+"$@"}' ;;
     (--t3st-eval=*) eval "${1#*=}"; shift
-      TTT__tfile_runme "$@" ;;
+      TTT__tfile_runme ${1+"$@"} ;;
+    (--) shift
+      TTT__tfile_runme ${1+"$@"} ;;
     (*)
-      set -e; TTT__tfile_setup "$@"; set +e
+      set -e; TTT__tfile_setup ${1+"$@"}; set +e
       k9s0ke_t3st_enter
-      TTT__tfile_tests "$@"
+      TTT__tfile_tests ${1+"$@"}
       k9s0ke_t3st_leave ;;
   esac
 }
-TTT__tfile_runme() { TTT__tfile_runme_0 "$@"; }
+TTT__tfile_runme() { TTT__tfile_runme_0 ${1+"$@"}; }
